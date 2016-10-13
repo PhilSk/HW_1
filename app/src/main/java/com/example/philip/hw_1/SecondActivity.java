7818e2928439c92114015d3383158fd78a8cc17c
PhilSk <fscomorohov@rambler.ru>
2016-10-13 11:47:37 +0300

    numbers left

--- app/src/main/java/com/example/philip/hw_1/SecondActivity.java
+++ app/src/main/java/com/example/philip/hw_1/SecondActivity.java
@@ -5,12 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class SecondActivity extends AppCompatActivity {
 
     Button button;
     TextView tw;
     Thread counterThread;
+    AtomicBoolean button_bool = new AtomicBoolean(true);
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,17 +23,45 @@ public class SecondActivity extends AppCompatActivity {
         tw = (TextView) findViewById(R.id.tw);
     }
 
+    @Override
+    protected void onStop() {
+        if (counterThread != null) {
+            counterThread.interrupt();
+        }
+        super.onStop();
+    }
+
+    private boolean button_checker() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (button.getText().equals("Start")){
+                    button_bool.set(true);
+                } else {
+                    button_bool.set(false);
+                }
+            }
+        });
+        return button_bool.get();
+    }
+
     public void count(View view) {
 
-        if (button.getText().equals("Start")) {
+        if (button_checker()) {
 
-            button.setText(R.string.stop);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                  button.setText(R.string.stop);
+                }
+            });
+
             (counterThread = new Thread(new Runnable() {
                 @Override
                 public void run() {
                     for (int i = 0; i <= 1000; ++i) {
                         try {
-                            if (button.getText().equals("Start")) { // stop counting on pressing "Stop"
+                            if (!button_bool.get()) { // stop counting on pressing "Stop"
                                 return;
                             } else {
                                 final int counter = i;
@@ -40,7 +70,6 @@ public class SecondActivity extends AppCompatActivity {
                                     @Override
                                     public void run() {
                                         tw.setText(String.valueOf(counter));
-
                                     }
                                 });
                             }
@@ -58,9 +87,16 @@ public class SecondActivity extends AppCompatActivity {
                 }
             })).start();
         } else {
-            button.setText(R.string.start);
-            tw.setText("");
-            counterThread.interrupt();
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    button.setText(R.string.start);
+                    tw.setText("");
+                }
+            });
+            if (counterThread != null) {
+                counterThread.interrupt();
+            }
         }
     }
 }
